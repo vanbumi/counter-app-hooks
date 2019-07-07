bd1f0d45e1c392908c7ddbe67f583c51348390a7
vanbumi <jsp.dio@gmail.com>
2019-07-07 16:11:50 +0700

    modified

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CounterHooks() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      hellooo
+      <h3>The Count is: ... <span><h1>{count}</h1></span></h3>
+      <button onClick={() => setCount(count + 1)}>Add count</button>
     </div>
   )
 }
